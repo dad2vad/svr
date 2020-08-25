b57a60d0480f43a966071b912c316fac6d86ed41
dad <60132399+dad2vad@users.noreply.github.com>
2020-08-25 13:40:50 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,14 @@ import 'firebase/auth'
 import { Fuego, FuegoProvider } from '@nandorojo/swr-firestore'
 
 const firebaseConfig = {
-  // put yours here
+   "apiKey":"AIzaSyCWHV-59Nzhr050jL_Uk0kDQU56hmYRLS8",
+   "authDomain":"vsamaru.firebaseapp.com",
+   "databaseURL":"https://vsamaru.firebaseio.com",
+   "projectId":"vsamaru",
+   "storageBucket":"vsamaru.appspot.com",
+   "messagingSenderId":"685290667281",
+   "appId":"1:685290667281:web:469d5dd61070d5404460b0",
+   "measurementId":"G-86X0K1BHN5"
 }
 
 const fuego = new Fuego(firebaseConfig)
